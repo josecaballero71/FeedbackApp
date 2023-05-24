2e65522ea95188f92135805629238e5cb9c04a9a
Manuel Caballero <manu_cabarosa32@outlook.com>
2023-05-24 13:31:55 -0500

    App Connected to Azure SQL DB

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,6 @@ dotenv.config();
 
 export default {
     port: process.env.PORT || 3000,
-    dbPort: process.env.DB_PORT || 1433,
     user: process.env.DB_USER || "",
     password: process.env.DB_PASSWORD || "",
     server: process.env.DB_SERVER || "",
